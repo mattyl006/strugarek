52b4a6d49b3f6f0c5751f34f7d66e3d46ae9b3ea
Mateusz <wirus006@gmail.com>
2021-08-06 18:00:20 +0200

    photos.js script alpha

---
+++ src/js/photos.js
@@ -0,0 +1,68 @@
+const arrowLeft = document.querySelector('.photos__arrow-left--js');
+const arrowRight = document.querySelector('.photos__arrow-right--js');
+
+const photos = document.querySelector('.photos__container--js');
+
+const handleLeftClick = () => {
+    if (!photos.classList.contains('photos-animation'))
+        photos.classList.add('photos-animation');
+
+    if(!photos.classList.contains('left1')
+        && !photos.classList.contains('right1')
+        && !photos.classList.contains('right2')
+        && !photos.classList.contains('right3')
+        && !photos.classList.contains('right4')
+        && !photos.classList.contains('right5')) {
+        photos.classList.add('left1');
+        return;
+    }
+
+    if(!photos.classList.contains('left2') && photos.classList.contains('left1')) {
+        photos.classList.add('left2');
+        return;
+    }
+
+    if(!photos.classList.contains('left3') && photos.classList.contains('left2')) {
+        photos.classList.add('left3');
+        return;
+    }
+
+    if(!photos.classList.contains('left4') && photos.classList.contains('left3')) {
+        photos.classList.add('left4');
+        return;
+    }
+}
+
+const handleRightClick = () => {
+    if (!photos.classList.contains('photos-animation'))
+        photos.classList.add('photos-animation');
+
+    if(!photos.classList.contains('right1')
+        && !photos.classList.contains('left1')
+        && !photos.classList.contains('left2')
+        && !photos.classList.contains('left3')
+        && !photos.classList.contains('left4')
+        && !photos.classList.contains('left5')) {
+        photos.classList.add('right1');
+        return;
+    }
+
+    if(!photos.classList.contains('right2') && photos.classList.contains('right1')) {
+        photos.classList.add('right2');
+        return;
+    }
+
+    if(!photos.classList.contains('right3') && photos.classList.contains('right2')) {
+        photos.classList.add('right3');
+        return;
+    }
+
+    if(!photos.classList.contains('right4') && photos.classList.contains('right3')) {
+        photos.classList.add('right4');
+        return;
+    }
+}
+
+arrowLeft.addEventListener('click', handleLeftClick);
+
+arrowRight.addEventListener('click', handleRightClick);
--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: {
     index: "./src/js/index.js",
     hamburger: "./src/js/hamburger.js",
+    photos: "./src/js/photos.js",
   },
   output: {
     filename: "[name].[hash:8].js",
@@ -79,7 +80,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/pages/index.html",
       inject: true,
-      chunks: ["index", "hamburger"],
+      chunks: ["index", "hamburger", "photos"],
       filename: "index.html",
     }),
   ],

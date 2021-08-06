50dc5ef4feb89f62eea7da57f60f097065f078f1
Mateusz <wirus006@gmail.com>
2021-08-07 01:15:43 +0200

    photos section

--- src/js/photos.js
+++ src/js/photos.js
@@ -31,6 +31,29 @@ const handleLeftClick = () => {
         photos.classList.add('left4');
         return;
     }
+
+    if(photos.classList.contains('right1') && !photos.classList.contains('right2')) {
+        photos.classList.remove('right1');
+        return;
+    }
+
+    if(photos.classList.contains('right2') && !photos.classList.contains('right3')) {
+        photos.classList.remove('right2');
+        return;
+    }
+
+    if(photos.classList.contains('right3') && !photos.classList.contains('right4')) {
+        photos.classList.remove('right3');
+        return;
+    }
+
+    if(photos.classList.contains('right4') && !photos.classList.contains('right5')) {
+        photos.classList.remove('right4');
+    }
+
+    if(photos.classList.contains('right5')) {
+        photos.classList.remove('right5');
+    }
 }
 
 const handleRightClick = () => {
@@ -60,6 +83,30 @@ const handleRightClick = () => {
     if(!photos.classList.contains('right4') && photos.classList.contains('right3')) {
         photos.classList.add('right4');
         return;
+    }
+
+    if(!photos.classList.contains('right5') && photos.classList.contains('right4')) {
+        photos.classList.add('right5');
+        return;
+    }
+
+    if(photos.classList.contains('left1') && !photos.classList.contains('left2')) {
+        photos.classList.remove('left1');
+        return;
+    }
+
+    if(photos.classList.contains('left2') && !photos.classList.contains('left3')) {
+        photos.classList.remove('left2');
+        return;
+    }
+
+    if(photos.classList.contains('left3') && !photos.classList.contains('left4')) {
+        photos.classList.remove('left3');
+        return;
+    }
+
+    if(photos.classList.contains('left4')) {
+        photos.classList.remove('left4');
     }
 }
 

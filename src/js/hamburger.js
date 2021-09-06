9d4704bcabfeff9e3f053fba70ec0d1a87ba9730
Mateusz <wirus006@gmail.com>
2021-09-06 11:43:45 +0200

    hide mobile menu when click main

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector('.hamburger--js');
 const menu = document.querySelector('.menu--js');
+const main = document.querySelector('.main--js');
 
 const handleHamburgerClick = () => {
     hamburger.classList.toggle('hamburger--active');
@@ -10,6 +11,13 @@ const handleMenuClick = () => {
     menu.classList.toggle('menu--visible');
 }
 
+const handleMainClick = () => {
+    menu.classList.remove('menu--visible');
+    hamburger.classList.toggle('hamburger--active');
+}
+
 hamburger.addEventListener('click', handleHamburgerClick);
 
 hamburger.addEventListener('click', handleMenuClick);
+
+main.addEventListener('click', handleMainClick);
